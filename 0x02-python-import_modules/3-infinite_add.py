26fabbf7143efec9c732a013b1eb672e4e7aa47f
SEBLEWONGEL GIRMA BAYU <99327556+SEBLEWONGELG@users.noreply.github.com>
2022-06-02 22:34:42 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
+    import sys
     result = 0
-    for i in argv[1:]:
-        result += int(i)
-    print(result)
-    
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))    
