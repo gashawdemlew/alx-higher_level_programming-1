3fcb66487861d23ffbf88b3d10603af59831cf54
SEBLEWONGEL GIRMA BAYU <99327556+SEBLEWONGELG@users.noreply.github.com>
2022-06-02 22:35:58 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-import hidden_4
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    for name in names:
-        if name[0] != '_':
-            print(name)
-            
+    import hidden_4
+    temp = dir(hidden_4)
+    for i in range(len(temp)):
+        for j in range(len(temp[i])):
+            if (temp[i][j] == '_' and temp[i][j+1] == '_'):
+                break
+            else:
+                print(temp[i])
+                break            
